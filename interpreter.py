6842bd1dcec32d2ecb32d9027cd88d0f6839351b
Carl Shan <shan.carl@gmail.com>
2016-09-09 08:48:38 -0700

    Adding the interpreter.py file

---
+++ interpreter.py
@@ -0,0 +1,109 @@
+what_to_execute = {
+    "instructions": [
+                     ("LOAD_VALUE", 0),
+                     ("LOAD_VALUE", 1),
+                     ("ADD_TWO_VALUES", None),
+                     ("PRINT_ANSWER", None)
+                    ],
+    "numbers": [7, 5]
+}
+
+class Interpreter:
+    def __init__(self):
+        self.stack = []
+        self.environment = {}
+
+    def LOAD_VALUE(self, number):
+        self.stack.append(number)
+
+    def PRINT_ANSWER(self):
+        answer = self.stack.pop()
+        print(answer)
+
+    def parse_arguments(self, instruction, argument, what_to_execute):
+        """Understands what the argument to each instruciton means."""
+        numbers = ['LOAD_VALUE']
+        names = ['LOAD_NAME', 'STORE_NAME']
+
+        if instruction in numbers:
+            argument = what_to_execute['numbers'][argument]
+        if instruction in names:
+            argument = what_to_execute['names'][argument]
+
+        return argument
+
+    def ADD_TWO_VALUES(self):
+        first_num = self.stack.pop()
+        second_num = self.stack.pop()
+        total = first_num + second_num
+        self.stack.append(total)
+
+    def STORE_NAME(self, name):
+        val = self.stack.pop()
+        self.environment[name] = val
+
+    def LOAD_NAME(self, name):
+        val = self.environment[name]
+        self.stack.append(val)
+
+
+    def run_code(self, what_to_execute):
+        """
+        This function takes in a dictionary, loops over each element in the instructions
+        processes arguments to the instructions, and then calls the corresponding method
+        on the interpreter object.
+        """
+        instructions = what_to_execute["instructions"]
+        for each_step in instructions:
+            instruction, argument = each_step
+            argument = self.parse_arguments(instruction, argument, what_to_execute)
+            if instruction == "LOAD_VALUE":
+                self.LOAD_VALUE(argument)
+            elif instruction == "ADD_TWO_VALUES":
+                self.ADD_TWO_VALUES()
+            elif instruction == "PRINT_ANSWER":
+                self.PRINT_ANSWER()
+            elif instruction == "STORE_NAME":
+                self.STORE_NAME(argument)
+            elif instruction == "LOAD_NAME":
+                self.LOAD_NAME(argument)
+
+    def execute(self, what_to_execute):
+        instructions = what_to_execute["instructions"]
+        for each_step in instructions:
+            instruction, argument = each_step
+            argument = self.parse_arguments(instruction, argument, what_to_execute)
+            bytecode_method = getattr(self, instruction)
+            if argument is None:
+                bytecode_method()
+            else:
+                bytecode_method(argument)
+
+interpreter = Interpreter()
+what_to_execute = {
+    "instructions": [("LOAD_VALUE", 0),
+                     ("LOAD_VALUE", 1),
+                     ("ADD_TWO_VALUES", None),
+                     ("LOAD_VALUE", 2),
+                     ("ADD_TWO_VALUES", None),
+                     ("PRINT_ANSWER", None)],
+    "numbers": [7, 5, 8] }
+
+# The below series of instructions correspond to the following code:
+# def s():
+# a = 1
+# b = 2
+# return a + b
+# This is turned into a series of instructions (or bytecode) by a compiler
+what_to_execute = {
+    "instructions": [("LOAD_VALUE", 0),
+                     ("STORE_NAME", 0),
+                     ("LOAD_VALUE", 1),
+                     ("STORE_NAME", 1),
+                     ("LOAD_NAME", 0),
+                     ("LOAD_NAME", 1),
+                     ("ADD_TWO_VALUES", None),
+                     ("PRINT_ANSWER", None)],
+    "numbers": [1, 2],
+    "names":   ["a", "b"] }
+interpreter.execute(what_to_execute)
